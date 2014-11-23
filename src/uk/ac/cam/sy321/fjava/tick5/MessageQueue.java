0b19c4dc605e08109a005641f9a1ce5aa703b72f
dangercrow <dangercrow@gmail.com>
2014-11-23 11:01:18 +0000

    Tick 5 Barebones
    Classes required (unmodified) from Tick 4 copied into Tick 5

---
+++ src/uk/ac/cam/sy321/fjava/tick5/MessageQueue.java
@@ -0,0 +1,7 @@
+package uk.ac.cam.sy321.fjava.tick5;
+
+public interface MessageQueue<T> {//A FIFO queue of items of type T
+	public abstract void put(T msg); //place msg on back of queue
+	public abstract T take(); //block until queue length >0; return head of queue
+}
+
---
+++ src/uk/ac/cam/sy321/fjava/tick5/SafeMessageQueue.java
@@ -0,0 +1,37 @@
+package uk.ac.cam.sy321.fjava.tick5;
+
+public class SafeMessageQueue<T> implements MessageQueue<T> {
+	private static class Link<L> {
+		L val;
+		Link<L> next;
+		Link(L val) { this.val = val; this.next = null; }
+	}
+	private Link<T> first = null;
+	private Link<T> last = null;
+	public synchronized void put(T val) {
+		Link<T> newLink = new Link<T>(val);
+		if(first==null){
+			first = newLink;
+			last = newLink;
+		}
+		else{
+			last.next = newLink; 
+			last = newLink;
+		}
+		this.notify();
+	}
+	public synchronized T take() {
+		while(first == null) //use a loop to block thread until data is available
+			try {
+				this.wait();
+			}
+		catch(InterruptedException ie) {}
+		T val = first.val;
+		first = first.next;
+		if (first == null){
+			last = null;
+		}
+		return val;
+
+	}
+}
